5a1d42ba6c3a5f688c25062c4434bfe451352096
vlad3k <vlad3k@bk.ru>
2020-10-02 18:51:34 +0300

    routing for page components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import PageMain from "../page-main/page-main";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import PageMain from "../pages/page-main/page-main";
+import PageFavorites from "../pages/page-favorites/page-favorites";
+import PageLogin from "../pages/page-login/page-login";
+import PageRoom from "../pages/page-room/page-room";
 
 const App = ({places}) => {
   return (
-    <PageMain places={places} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <PageMain places={places} />
+        </Route>
+        <Route path="/favorites" exact>
+          <PageFavorites />
+        </Route>
+        <Route path="/login" exact>
+          <PageLogin places={places} />
+        </Route>
+        <Route path="/offer/:id?" exact>
+          <PageRoom />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
